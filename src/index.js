3a12ef535b8521bb913bd9f89c058565943afc41
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-06-28 13:55:38 +0200

    Add actions and reducer for expenses

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import * as serviceWorker from './serviceWorker';
+
+const store = configureStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
