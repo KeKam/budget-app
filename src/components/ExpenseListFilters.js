1844ee6667088601575e975f910326bf7431f6d2
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-07-04 14:33:02 +0200

    Add onChange handler to input field

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { setTextFilter } from '../actions/filters';
 
-const ExpenseListFilters = ({ filters }) => {
+const ExpenseListFilters = ({ filters, dispatch }) => {
   return (
     <div>
-      <input type='text' value={filters.text} />
+      <input type='text' value={filters.text} onChange={(e) => {
+        dispatch(setTextFilter(e.target.value));
+      }} />
     </div>
   );
 };
