5b2b4536acf658fc1e8a485326d9eab8e3826e94
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-07-23 13:45:37 +0200

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selector/expenses';
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'New laptop', amount: 5500 }));
-store.dispatch(addExpense({ description: 'New pants', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'New shoes', amount: 4000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
