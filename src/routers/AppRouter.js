b2de537e5c89b75bfa405de3e568284d8a33ddc8
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-08-20 14:00:48 +0200

    Add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Header from '../components/Header';
 import Dashboard from '../components/Dashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import NotFound from '../components/NotFound';
 import Login from '../components/Login';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createBrowserHistory();
 
@@ -14,12 +14,11 @@ const AppRouter = () => {
   return (
     <Router history={history}>
       <div>
-        <Header />
         <Switch>
           <Route path='/' component={Login} exact={true} />
-          <Route path='/dashboard' component={Dashboard} />
-          <Route path='/create' component={AddExpense} />
-          <Route path='/edit/:id' component={EditExpense} />
+          <PrivateRoute path='/dashboard' component={Dashboard} />
+          <PrivateRoute path='/create' component={AddExpense} />
+          <PrivateRoute path='/edit/:id' component={EditExpense} />
           <Route component={NotFound} />
         </Switch>
       </div>
