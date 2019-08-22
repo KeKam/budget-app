8bd44f83a5ebca91c6552c1105fbcbb569e410bb
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-08-22 14:30:43 +0200

    Update expenses actions to handle user id

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -6,7 +6,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = '',
       note = '',
@@ -15,7 +16,7 @@ export const startAddExpense = (expenseData = {}) => {
     } = expenseData;
     const expense = { description, note, amount, createdAt };
     try {
-      const response = await database.ref('expenses').push(expense);
+      const response = await database.ref(`users/${uid}/expenses`).push(expense);
       dispatch(addExpense({
         id: response.ref.key,
         ...expense
@@ -32,9 +33,10 @@ export const removeExpense = ({ id }) =>({
 });
 
 export const startRemoveExpense = ({ id } = {}) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const uid = getState().auth.uid;
     try {
-      await database.ref(`expenses/${id}`).remove();
+      await database.ref(`users/${uid}/expenses/${id}`).remove();
       dispatch(removeExpense({ id }));
     } catch (error) {
       console.log('Failed to remove expense', error);
@@ -49,9 +51,10 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const uid = getState().auth.uid;
     try {
-      await database.ref(`expenses/${id}`).update(updates);
+      await database.ref(`users/${uid}/expenses/${id}`).update(updates);
       dispatch(editExpense(id, updates));
     } catch (error) {
       console.log('Failed to update expense', error);
@@ -65,9 +68,10 @@ export const setExpenses = (expenses) => ({
 });
 
 export const startSetExpenses = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const uid = getState().auth.uid;
     try {
-      const response = await database.ref('expenses').once('value');
+      const response = await database.ref(`users/${uid}/expenses`).once('value');
       const expenses = [];
       response.forEach((expense) => {
         expenses.push({
