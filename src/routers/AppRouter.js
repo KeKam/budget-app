f0721c27f191a2b3559e08617c110ed8ee24adb7
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-08-27 14:40:19 +0200

    Remove margin on body

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { createGlobalStyle } from 'styled-components'
 import Dashboard from '../components/Dashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
@@ -9,12 +10,19 @@ import Login from '../components/Login';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
 export const history = createBrowserHistory();
 
 const AppRouter = () => {
   return (
     <Router history={history}>
       <div>
+        <GlobalStyle />
         <Switch>
           <PublicRoute path='/' component={Login} exact={true} />
           <PrivateRoute path='/dashboard' component={Dashboard} />
