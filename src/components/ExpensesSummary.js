0abfe34ca42ad8cd8170d57e2b54c0e127af8154
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-09-02 14:37:12 +0200

    Add styling to ExpensesSummary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
+import styled from 'styled-components';
 import getVisibleExpenses from '../selector/expenses';
 import totalExpenses from '../selector/expenses-total';
+import { ContentContainer } from '../styles/styles';
+
+const SummaryArea = styled.div`
+  background: #f5f5f5;
+  margin-bottom: 2rem;
+  padding: 2rem 0;
+`;
+
+const SummaryText= styled.h1`
+  font-weight: 300;
+  margin: 0;
+
+  span {
+    font-weight: 700;
+  }
+`;
+
+const SummaryNavigation = styled.div`
+  margin-top: 1.6rem;
+`;
+
+const StyledLink = styled(Link)`
+  background: #4078c0;
+  color: white;
+  display: inline-block;
+  font-size: 1.2rem;
+  font-weight: 300;
+  padding: 0.5rem;
+  cursor: pointer;
+  text-decoration: none;
+  line-height: 1;
+`;
 
 const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
   const formattedTotalValue = numeral(expensesTotal / 100).format('$0,0.00');
   return (
-    <div>
-      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedTotalValue} </h1>
-    </div>
+    <SummaryArea>
+      <ContentContainer>
+        <SummaryText>Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedTotalValue}</span> </SummaryText>
+        <SummaryNavigation>
+          <StyledLink to='/create'>Add Expense</StyledLink>
+        </SummaryNavigation>
+      </ContentContainer>
+    </SummaryArea>
   );
 };
 
