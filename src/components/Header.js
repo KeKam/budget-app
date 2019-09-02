e2f100d699172d5d3ca7b7d3112be9b12de41769
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-09-02 13:53:07 +0200

    Add styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import { startLogout } from '../actions/auth';
+import { ContentContainer } from '../styles/styles';
+
+const StyledHeader = styled.header`
+  background: #364051;
+  color: white;
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.2rem 0;
+`;
+
+const HeaderLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const HeaderTitle = styled.h1`
+  color: white;
+  text-decoration: none;
+  margin: 0;
+`;
 
 const Header = ({ startLogout }) => {
   return (
-    <header>
-      <h1>Budget App</h1>
-      <NavLink to='/dashboard'>Dashboard</NavLink>
-      <NavLink to='/create'>Add Expense</NavLink>
-      <button onClick={startLogout}>Logout</button>
-    </header>
+    <StyledHeader>
+      <ContentContainer>
+        <HeaderContent>
+          <HeaderLink to='/dashboard'>
+            <HeaderTitle>Budgetify</HeaderTitle>
+          </HeaderLink>
+          <button onClick={startLogout}>Logout</button>
+        </HeaderContent>
+      </ContentContainer>
+    </StyledHeader>
   );
 };
 
---
+++ src/styles/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ContentContainer = styled.div`
+  margin: 0 auto;
+  padding: 0 1.6rem;
+  max-width: 80rem;
+`;
