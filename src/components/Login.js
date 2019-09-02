c39e1cfa573bc39b84ae9e9b2d153ad3e03bc852
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-09-02 13:53:59 +0200

    Add missing semicolons

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const LoginScreen = styled.div`
   justify-content: center;
   height: 100vh;
   width: 100vw;
-`
+`;
 
 const LoginPanel = styled.div`
   background: #f5f5f5;
@@ -20,11 +20,13 @@ const LoginPanel = styled.div`
   text-align: center;
   width: 15rem;
   opacity: 0.9;
-`
+`;
+
 const LoginPanelTitle = styled.h1`
   margin: 0 0 1.2rem 0;
   line-height: 1;
-`
+`;
+
 const LoginPanelButton = styled.button`
   background: #4078c0;
   border: none;
@@ -33,7 +35,7 @@ const LoginPanelButton = styled.button`
   font-weight: 300;
   padding: 0.5rem;
   cursor: pointer;
-`
+`;
 
 const Login = ({ startLogin }) => {
   return (
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 import Dashboard from '../components/Dashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
